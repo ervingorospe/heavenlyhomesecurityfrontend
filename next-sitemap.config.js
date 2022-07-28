6ca5cbb09e09c46b6dcc5d1d6f3aefe7f6e51122
ervingorospe <ervingorospe123@gmail.com>
2022-07-28 18:41:49 +0800

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: 'https://fascinating-croquembouche-171853.netlify.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+
+module.exports = config
